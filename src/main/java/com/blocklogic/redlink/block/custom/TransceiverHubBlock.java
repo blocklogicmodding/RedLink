fb75db42740937a8fe326e77aefb61cdd222d76a
deonjonker123 <deon.jonker@hotmail.com>
2025-07-09 22:21:50 +0200

    Initial blocks and items implemented

--- src/main/java/com/blocklogic/redlink/block/custom/TransceiverHubBlock.java
+++ src/main/java/com/blocklogic/redlink/block/custom/TransceiverHubBlock.java
@@ -3,14 +3,10 @@ package com.blocklogic.redlink.block.custom;
 import com.blocklogic.redlink.block.entity.TransceiverHubBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
 public class TransceiverHubBlock extends BaseEntityBlock {
@@ -28,17 +24,6 @@ public class TransceiverHubBlock extends BaseEntityBlock {
     @Override
     protected RenderShape getRenderShape(BlockState state) {
         return RenderShape.MODEL;
-    }
-
-    @Override
-    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
-        if (!level.isClientSide()) {
-            BlockEntity entity = level.getBlockEntity(pos);
-            if (entity instanceof TransceiverHubBlockEntity) {
-                player.openMenu((TransceiverHubBlockEntity) entity, pos);
-            }
-        }
-        return InteractionResult.sidedSuccess(level.isClientSide());
     }
 
     @Nullable
